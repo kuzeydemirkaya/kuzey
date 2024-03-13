d691540d386039a66a5b6008f5c7ff450adfb26e
Kuzey Demirkaya <86108320+kuzeydemirkaya@users.noreply.github.com>
2024-03-13 16:00:55 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,10 @@
-const toggleSwitch = document.querySelector('.toggle-checkbox');
-const header = document.querySelector('.header');
-const footer = document.querySelector('.footer');
-const logo = document.querySelector('.logo iframe');
-
-toggleSwitch.addEventListener('change', switchTheme);
-
-function switchTheme() {
-    if (toggleSwitch.checked) {
-        document.body.classList.add('dark-mode');
-        header.classList.add('dark-mode');
-        footer.classList.add('dark-mode');
-        // Change logo source
+function toggleDarkMode() {
+    const body = document.body;
+    const logo = document.querySelector('.logo iframe');
+    body.classList.toggle('dark-mode');
+    if (body.classList.contains('dark-mode')) {
         logo.src = "https://giphy.com/embed/kOvPo6vScx3cM1xu81";
     } else {
-        document.body.classList.remove('dark-mode');
-        header.classList.remove('dark-mode');
-        footer.classList.remove('dark-mode');
-        // Change logo source back to light mode
         logo.src = "https://giphy.com/embed/9J3zCEDkHaifAOgJlO";
     }
 }
